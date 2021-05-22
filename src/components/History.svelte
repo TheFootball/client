520fd86eaa01e850112de8eb894b6a6a4c1cd8ef
changhoi <0417kch@naver.com>
2021-05-22 23:10:39 +0900

    feat(history): set history page

---
+++ src/components/History.svelte
@@ -0,0 +1,15 @@
+
+<div class="history-wraper">
+    <h1>History</h1>
+</div>
+
+<style lang="scss">
+    .history-wrapper {
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        & > h1 {
+            align-self: center;
+        }
+    }
+</style>
