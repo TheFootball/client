241f70409e2789e2ce903753eafc29ce92a0d27f
neulhan <sting7675@gmail.com>
2021-05-23 10:43:37 +0900

    feat: create ending scene

---
+++ src/pages/Ending.svelte
@@ -0,0 +1,46 @@
+<script>
+  export let params = {}
+  console.log(params)
+</script>
+
+<div class="ending" style="background-image: url(../assets/images/ending-{params.result}.svg)">
+  <a href="#/" class="restart-button">
+    <img src="/assets/images/restart.svg" alt="" />
+    Restart
+  </a>
+</div>
+
+<style lang="scss">
+  .ending {
+    width: 100%;
+    height: 100vh;
+
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    .restart-button {
+      position: fixed;
+      right: 48px;
+      bottom: 48px;
+      background-color: white;
+      width: 200px;
+      height: 70px;
+      border-radius: 10px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: #ff9b40;
+      font-size: 25px;
+      font-weight: bold;
+      text-decoration: none;
+      img {
+        margin-right: 8px;
+      }
+    }
+  }
+</style>
